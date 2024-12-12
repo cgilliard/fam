276b55f762b8da8a5f1cdb32d83d1f123c4a0d5e
Christopher Gilliard <christopher.gilliard@gmail.com>
2024-12-12 15:13:20 -0800

    improve coverage script

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -5,5 +5,68 @@ ar rcs libtest.a *.o || exit 1;
 rustc --test rust/mod.rs -C instrument-coverage -C opt-level=0 -o bin/test_fam -L . -l static=test || exit 1;
 export LLVM_PROFILE_FILE="/tmp/file.profraw"
 ./bin/test_fam || exit 1;
-grcov /tmp/file.profraw --branch --binary-path ./bin --llvm-path=/Users/christophergilliard/homebrew/opt/llvm/bin > /tmp/coverage.txt
+git log -1 > /tmp/coverage.txt || exit 1;
+grcov \
+	/tmp/file.profraw \
+	--branch --binary-path \
+	./bin \
+	--llvm-path=/Users/christophergilliard/homebrew/opt/llvm/bin > /tmp/coverage.txt || exit 1;
+
+cur_file='';
+line_count=0;
+cov_count=0;
+line_count_sum=0;
+cov_count_sum=0;
+
+if [ "$NO_COLOR" = "" ]; then
+   GREEN="\033[32m";
+   CYAN="\033[36m";
+   YELLOW="\033[33m";
+   BRIGHT_RED="\033[91m";
+   RESET="\033[0m";
+   BLUE="\033[34m";
+else
+   GREEN="";
+   CYAN="";
+   YELLOW="";
+   BRIGHT_RED="";
+   RESET="";
+   BLUE="";
+fi
+
+echo "Code Coverage Report for commit: $GREEN`git log -1 | grep "^commit " | cut -f2 -d ' '`$RESET";
+echo "$BLUE----------------------------------------------------------------------------------------------------$RESET";
+
+for line in $(cat /tmp/coverage.txt)
+do
+	if [ "`echo $line | grep "^SF:"`" != "" ]; then
+		cur_file=`echo $line | cut -f2 -d ':'`;
+	fi
+	if [ "`echo $line | grep "^DA:"`" != "" ]; then
+		#echo "da: $line";
+		line_count=$((1 + line_count));
+		line_count_sum=$((1 + line_count_sum));
+		if [ "`echo $line | cut -f2 -d ','`" != "0" ]; then
+			cov_count=$((1 + cov_count));
+			cov_count_sum=$((1 + cov_count_sum));
+		fi
+	fi
+	if [ "$line" = "end_of_record" ]; then
+		percent=100;
+		if [ "$line_count" != "0" ]; then
+			percent=$(((cov_count * 100) / line_count));
+		fi
+		line_fmt="($cov_count/$line_count)";
+		printf "Cov: $GREEN%3s%%$RESET Lines: $YELLOW%10s$RESET File: $CYAN%s$RESET\n" "$percent" "$line_fmt" "$cur_file"
+		line_count=0;
+		cov_count=0;
+	fi
+done
+echo "$BLUE----------------------------------------------------------------------------------------------------$RESET";
+
+percent=100;
+if [ "$line_count_sum" != "0" ]; then
+	percent=$(((cov_count_sum * 100) / line_count_sum));
+fi
+echo "Summary: $GREEN$percent%$RESET Lines: $YELLOW($cov_count_sum/$line_count_sum)$RESET!"
 rm -f libtest.a *.o
