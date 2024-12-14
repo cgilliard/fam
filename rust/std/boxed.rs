fc87e2522f1539e9820161193b9d832b3d320cdb
Christopher Gilliard <christopher.gilliard@gmail.com>
2024-12-13 18:55:28 -0800

    impl clone for box

--- rust/std/boxed.rs
+++ rust/std/boxed.rs
@@ -1,30 +1,49 @@
 use core::mem::{drop, size_of};
 use core::ops::Drop;
 use core::ptr;
+use core::ptr::copy_nonoverlapping;
 use err;
+use std::clone::Clone;
 use std::error::{Error, ErrorKind::Alloc};
 use std::result::{Result, Result::Err, Result::Ok};
 use sys::{map, unmap};
 
 pub struct Box<T> {
 	value: *mut T,
-	pages: usize,
 }
 
 impl<T> Drop for Box<T> {
 	fn drop(&mut self) {
+		let pages = pages!(size_of::<T>());
 		unsafe {
 			let value = ptr::read(self.value);
 			drop(value);
-			unmap(self.value as *mut u8, self.pages);
+			unmap(self.value as *mut u8, pages);
 		}
+	}
+}
+
+impl<T> Clone for Box<T> {
+	fn clone(&self) -> Result<Self, Error> {
+		let value = unsafe { &mut *self.value };
+		let nvalue;
+		let pages = pages!(size_of::<T>());
+		unsafe {
+			nvalue = map(pages) as *mut T;
+			if nvalue.is_null() {
+				return Err(err!(Alloc));
+			}
+			copy_nonoverlapping(value, nvalue, size_of::<T>());
+		}
+
+		Ok(Self { value: nvalue })
 	}
 }
 
 impl<T> Box<T> {
 	pub fn new(t: T) -> Result<Self, Error> {
-		let pages = pages!(size_of::<T>());
 		let value;
+		let pages = pages!(size_of::<T>());
 		unsafe {
 			value = map(pages) as *mut T;
 			if value.is_null() {
@@ -33,10 +52,7 @@ impl<T> Box<T> {
 			ptr::write(value, t);
 		}
 
-		Ok(Self {
-			pages: pages as usize,
-			value,
-		})
+		Ok(Self { value })
 	}
 
 	pub fn as_ref(&self) -> &T {
@@ -51,6 +67,7 @@ impl<T> Box<T> {
 #[cfg(test)]
 mod test {
 	use std::boxed::Box;
+	use std::clone::Clone;
 	#[test]
 	fn test_box() {
 		let mut x = Box::new(4).unwrap();
@@ -60,5 +77,9 @@ mod test {
 		let z = x.as_mut();
 		*z = 10;
 		assert_eq!(*z, 10);
+
+		let a = x.clone().unwrap();
+		let b = a.as_ref();
+		assert_eq!(*b, 10);
 	}
 }
