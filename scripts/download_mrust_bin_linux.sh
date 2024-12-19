fe1b30a4fa8ed620964fed7179929b296f930445
Christopher Gilliard <christopher.gilliard@gmail.com>
2024-12-18 21:40:15 -0800

    refactor scripts

--- scripts/download_mrust_bin_linux.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-git clone https://github.com/cgilliard/rust-bins
-./fam --mrustc --with-mrustc=./rust-bins/linux/mrustc --output=./rust-bins/linux/output
--- scripts/download_mrust_bin_macos.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-git clone https://github.com/cgilliard/rust-bins
-./fam --mrustc --with-mrustc=./rust-bins/macos/mrustc --output=./rust-bins/macos/output
