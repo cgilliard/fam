951f97bdd1b0ae5978113e48e16506d7c091df77
Christopher Gilliard <christopher.gilliard@gmail.com>
2024-12-24 20:40:13 -0800

    add channel/lock/thread

--- rust/prelude.rs
+++ rust/prelude.rs
@@ -1,22 +1,19 @@
-/*
+pub use crate::exit;
 pub use crate::{
 	channel, format, lock, lock_box, lock_pair, print, print_num, println, rc, vec, writeb,
 };
-*/
-pub use crate::exit;
-pub use crate::{format, println, rc, vec, writeb};
 pub use std::boxed::Box;
-//pub use std::channel::Channel;
+pub use std::channel::Channel;
 pub use std::clone::Clone;
 pub use std::error::{Error, ErrorKind};
 pub use std::fmt::{Display, Formatter};
-pub use std::pointer::Pointer;
-//pub use std::lock::{Lock, LockBox};
+pub use std::lock::{Lock, LockBox};
 pub use std::option::{Option, Option::None, Option::Some};
+pub use std::pointer::Pointer;
 pub use std::rc::Rc;
 pub use std::result::{Result, Result::Err, Result::Ok};
 pub use std::string::String;
-//pub use std::thread::{spawn, spawnj};
+pub use std::thread::{spawn, spawnj};
 pub use std::vec::Vec;
 //pub use util::murmur::{murmur3_32_of_slice, MURMUR_SEED};
 
--- rust/std/mod.rs
+++ rust/std/mod.rs
@@ -1,13 +1,17 @@
+#[macro_use]
+pub mod macros;
+
 pub mod boxed;
+pub mod channel;
 pub mod clone;
 pub mod error;
 pub mod fmt;
-#[macro_use]
-pub mod macros;
+pub mod lock;
 pub mod option;
 pub mod pointer;
 pub mod rc;
 pub mod result;
 pub mod string;
+pub mod thread;
 pub mod util;
 pub mod vec;
