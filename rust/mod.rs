dba2cda255bfd6b692644d643d773140d5f84ffc
Christopher Gilliard <christopher.gilliard@gmail.com>
2024-12-30 10:16:20 -0800

    clean out

--- rust/mod.rs
+++ rust/mod.rs
@@ -1,15 +1,3 @@
 #![no_std]
-#![feature(unsize)]
-#![feature(coerce_unsized)]
-#![allow(internal_features)]
-#![feature(core_intrinsics)]
-#![no_implicit_prelude]
 
-#[macro_use]
-pub mod std;
-
-pub mod net;
-pub mod prelude;
 mod real_main;
-mod sys;
-pub mod util;
--- rust/real_main.rs
+++ rust/real_main.rs
@@ -1,7 +1,4 @@
-use prelude::*;
-
 #[no_mangle]
 pub extern "C" fn real_main(_argc: i32, _argv: *const *const u8) -> i32 {
-	println!("real main {}", _argc);
 	0
 }
